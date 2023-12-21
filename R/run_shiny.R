7e597918a3d2fbf18bf2837007ba639be6916f91
Zach-Levine <levinez@mcmaster.ca>
2023-12-21 15:34:13 +0200

    add gwas results for the MB and Metab

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -8,66 +8,20 @@ shiny_manhattan <- function(x, phenoName){
 
 }
 
-options_mb <-   read.csv(system.file("lists",
-                                     "mb_options.csv",
-                                     package = "gwasInterface"))
-list_kingdom <- function(){
-  c(unique(options_mb$kingdom))
-
-}
-list_phylum <- function(kingdom){
-  print(kingdom)
-  unique(options_mb[options_mb$kingdom == kingdom, "phylum"])
-}
-list_class <- function(kingdom, phylum){
-  temp <- options_mb[options_mb$kingdom == kingdom,]
-  unique(temp[temp$phylum == phylum, "class"])
-
-}
-list_order <- function(kingdom, phylum, class){
-  temp <- options_mb[options_mb$kingdom == kingdom,]
-  temp2 <- temp[temp$phylum == phylum,]
-  unique(temp2[temp2$class == "class","order"])
-}
-list_family <- functiony <- function(kingdom,phylum, class, order){
-  temp <- options_mb[options_mb$kingdom == kingdom,]
-  temp2 <- temp[temp$phylum == phylum,]
-  temp3 <- temp2[temp2$class == class,]
-  unique(temp[temp$order == order, "family"])
-}
-list_genus <- function(kingdom, phylum, class, order, family){
-  temp <- options_mb[options_mb$kingdom == kingdom,]
-  temp2 <- temp[temp$phylum == phylum,]
-  temp3 <- temp2[temp2$class == class,]
-  temp4 <- temp3[temp$order == order,]
-  unique(temp4[temp4$family == family, "genus"])
-
-}
-list_species <- function(kingdom, phylum, class,order, family, genus){
-  temp <- options_mb[options_mb$kingdom == kingdom,]
-  temp2 <- temp[temp$phylum == phylum,]
-  temp3 <- temp2[temp2$class == class,]
-  temp4 <- temp3[temp$order == order,]
-  temp5 <- temp4[temp4$family == family,]
-  unique(temp5[temp5$genus == genus, "species"])
-}
-
-find_fname <- function(kingdom, phylum, class,order, family, genus, species){
-  temp <- options_mb[options_mb$kingdom == kingdom,]
-  temp2 <- temp[temp$phylum == phylum,]
-  temp3 <- temp2[temp2$class == class,]
-  temp4 <- temp3[temp$order == order,]
-  temp5 <- temp4[temp4$family == family,]
-  temp6<- (temp5[temp5$genus == genus,])
-  temp6[[temp5$species == species,"X"]]
-  }
-
+options_mb <- read.csv(system.file("lists",
+                                     "MicrobiomeLoader.csv",package = "gwasInterface"))
 
 list_features <- function(domain){
-  read.csv(system.file("lists",
-                       paste0(domain ,"Loader", ".csv"),
-                       package = "gwasInterface"))[,2]
+  if (domain != "Microbiome"){
+    read.csv(system.file("lists",
+                         paste0(domain ,"Loader", ".csv"),
+                         package = "gwasInterface"))[,2]
+  }
+  else{
+    options_mb$species
+  }
 }
+
 
 make_paper_figures <- function(){
   qu <- read.csv("/net/mraid08/export/jasmine/zach/height_gwas/all_gwas/gwas_results/batch0.1st_Qu..glm.linear", sep = "\t")
@@ -162,7 +116,12 @@ read_clumped <- function(fname){
   read_in
 }
 
-format_gwas <- function(thephenoname){
+format_gwas <- function(thephenoname, domain){
+  if (domain == "Microbiome"){
+    thephenoname <- paste0(options_mb[options_mb$species == thephenoname, "ok"], ".glm.linear")
+  }
+  else{
+  }
   thegwas <- readRDS(system.file(paste0("full_results_rds/", thephenoname, ".Rds"),
                                  package = "gwasInterface"))
   colnames(thegwas) <- c("CHR", "BP", "SNP", "REF", "ALT", "A1", "AX", "TEST", "N", "BETA", "SE", "T_STAT", "P")
@@ -214,8 +173,7 @@ run_shiny <- function(useBrowser = TRUE, usingOnline = FALSE) {
                                        h4(id = "t0", "PRS Associations Heatmap (Corrected P)"),
                                        plotOutput("heatmap"),
                                        uiOutput("featureSelect"),
-                                       uiOutput("aux_featureSelect"),
-                                       uiOutput("gwasDownload")
+                                       uiOutput("gwasDownload"),
                                        ))),
                     mainPanel(
                         h3(id = "t1", "Manhattan Plot of GWAS Results"),
@@ -236,13 +194,18 @@ run_shiny <- function(useBrowser = TRUE, usingOnline = FALSE) {
                                              package = "gwasInterface"))
     prs_name_dict_loaded <- readRDS(system.file("prs_name_dict.Rds",
                                             package = "gwasInterface"))
+
+
     observeEvent(input[["pheno"]], {
       output$plot <- renderPlot({
         ##Force reactive loading based on these values
         if (input$domain == "Microbiome"){
-          actual_pheno_fname = find_fname <-find_fname(input$kingdom, input$phylum, input$class,input$order, input$family, input$genus,input$species)
+          if (is.null(input$pheno)){
+            input$pheno <- list_features(input$domain)[3]
+          }
+          actual_pheno_fname <- paste0(options_mb[options_mb$species == input$pheno, "ok"], ".glm.linear")
           shiny_manhattan(readRDS(system.file(paste0("full_results_rds/",actual_pheno_fname, ".Rds"),
-                                              package = "gwasInterface")), input$pheno)
+                                              package = "gwasInterface")), "Selected Microbiome Species")
         }
         else{
           shiny_manhattan(readRDS(system.file(paste0("full_results_rds/", input$pheno, ".Rds"),
@@ -251,80 +214,76 @@ run_shiny <- function(useBrowser = TRUE, usingOnline = FALSE) {
         })
 
       output$table <- renderDataTable({
-        thegwas <- format_gwas(input$pheno)
+        thegwas <- format_gwas(input$pheno, input$domain)
         })
       output$prs_assoc <- renderDataTable({
-        justpheno <- as.numeric(prs_table_loaded[prs_table_loaded$Phenotype == input$pheno,]) ##first two entries are NA corresponding to the index of Loader, Phenotype
-        sig_ids <- justpheno < 0.05
-        sig_ids[c(1,2)] <- c(FALSE, FALSE) ##set FALSE (instead of NA) to the first two columns because they are just the index
-        justpheno_sig_P <- justpheno[sig_ids]
-        justpheno_sig_prs_names <- colnames(prs_table_loaded)[sig_ids]
-        ##use a tibble so we can have spaces in the column names
-        res_table <- tibble("Phenotype" = rep(input$pheno, length(justpheno_sig_P)), "Corrected P Value" = justpheno_sig_P, "Polygenic Risk Score (PRS)" = justpheno_sig_prs_names)
-        res_table["Corresponding UK Biobank Phenotype Code for PRS"] <- sapply(res_table[["Polygenic Risk Score (PRS)"]], function(thename){prs_name_dict_loaded[prs_name_dict_loaded$name == thename,]$code})
-        res_table <- data.table(res_table[order(res_table["Corrected P Value"]), c("Phenotype", "Polygenic Risk Score (PRS)", "Corresponding UK Biobank Phenotype Code for PRS", "Corrected P Value")])
+        if (input$domain == "Metabolomics" | input$domain == "Microbiome"){
+          res_table <- data.table()
+        }
+        else{
+          justpheno <- as.numeric(prs_table_loaded[prs_table_loaded$Phenotype == input$pheno,]) ##first two entries are NA corresponding to the index of Loader, Phenotype
+          sig_ids <- justpheno < 0.05
+          sig_ids[c(1,2)] <- c(FALSE, FALSE) ##set FALSE (instead of NA) to the first two columns because they are just the index
+          justpheno_sig_P <- justpheno[sig_ids]
+          justpheno_sig_prs_names <- colnames(prs_table_loaded)[sig_ids]
+          ##use a tibble so we can have spaces in the column names
+          res_table <- tibble("Phenotype" = rep(input$pheno, length(justpheno_sig_P)), "Corrected P Value" = justpheno_sig_P, "Polygenic Risk Score (PRS)" = justpheno_sig_prs_names)
+          res_table["Corresponding UK Biobank Phenotype Code for PRS"] <- sapply(res_table[["Polygenic Risk Score (PRS)"]], function(thename){prs_name_dict_loaded[prs_name_dict_loaded$name == thename,]$code})
+          res_table <- data.table(res_table[order(res_table["Corrected P Value"]), c("Phenotype", "Polygenic Risk Score (PRS)", "Corresponding UK Biobank Phenotype Code for PRS", "Corrected P Value")])
+        }
       })
     })
 
     observeEvent(input[["domain"]], {
+      if (input$domain != "Metabolomics" & input$domain != "Microbiome"){
         output$heatmap <- renderImage({
           list(src=system.file(paste0("prs_heatmap_imgs/", input$domain, "Loader", ".png"),
                                package = "gwasInterface"),
                width = "80%",
                height = "100%")
         }, deleteFile = FALSE)
+      }
+      else{
+        output$heatmap <- renderImage({
+          list(src=system.file(paste0("prs_heatmap_imgs/", input$domain, "Loader", ".png"),
+                               package = "gwasInterface"),
+               width = "0%",
+               height = "0%")
+        }, deleteFile = FALSE)
+      }
     })
 
     output$gwasDownload <- renderUI({
       downloadButton("downloadData", "Download GWAS Summary Statistics", class = "dbutton")
     })
-    output$featureSelect <- renderUI({
-      if (input$domain != "Microbiome"){
 
-      }
+
+    output$featureSelect <- renderUI({
+      selectInput("pheno",
+                  label = "Phenotype:",
+                  choices = list_features(input$domain),
+                  selected = list_features(input$domain)[2])
 
     })
-    output$aux_featureSelect <- renderUI({
-        if (input$domain == "Microbiome"){
-        selectInput("kingdom",
-                    label = "Kingdom:",
-                    choices = list_kingdom(),
-                    selected = "Bacteria")
-          selectInput("phylum",
-                      label = "Phylum:",
-                      choices = list_phylum(input$kingdom))
 
-
-        selectInput("class",
-                    label = "Class:",
-                    choices = list_class(input$kingdom, input$phylum))
-        selectInput("order",
-                    label = "Order:",
-                    choices = list_order(input$kingdom, input$phylum, input$class))
-        selectInput("family",
-                    label = "Family:",
-                    choices = list_family(input$kingdom, input$phylum, input$class, input$order))
-        selectInput("genus",
-                    label = "Genus:",
-                    choices = list_genus(input$kingdom, input$phylum, input$class, input$order, input$family))
-        selectInput("species",
-                    label = "Species:",
-                    choices = list_species(input$kingdom, input$phylum, input$class, input$order, input$family, input$genus))
-        }
-      })
-
-    ##Handle downloads for the sample template csv file.
-    ##The file is an empty version of ICU1.csv so it scales as more parameters are added.
+    ##Handle downloads
     output$downloadData <- downloadHandler(
-      filename = function(){return(paste0(input$pheno, ".csv"))},
+      filename = function(){
+        if (input$domain == "Microbiome") {
+          paste0(options_mb[options_mb$species == input$pheno, "ok"], ".csv")
+        }
+        else{
+          paste0(input$pheno, ".csv")
+        }
+      },
       content = function(file) {
-        write.csv(format_gwas(input$pheno), file, row.names = FALSE)
+      write.csv(format_gwas(input$pheno, input$domain), file, row.names = FALSE)
       }
     )
       output$sourcelink <- renderUI({tagList(
       "Source code available at ",
       a("https://github.com/ZachLevine-11/gwasInterface", href = "https://github.com/ZachLevine-11/gwasInterface"),
-      "                                Based on the paper: Paper title"
+      "                                Based on the paper: Genome Wide Association Studies and Polygenic Risk Score Phenome Wide Association Studies across complex phenotypes in the Human Phenotype Project"
     )})
   }
     shinyApp(ui, server)
